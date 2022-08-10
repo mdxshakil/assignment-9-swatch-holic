ab1b16a8484334a053c86b4de6b63c2db4ac3be1
mdxshakil <mdxshakil1767@gmail.com>
2022-08-10 18:13:02 +0600

    project finished

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons'
 
 const Review = ({ review }) => {
     const { name, picture, comment } = review;
@@ -9,8 +11,15 @@ const Review = ({ review }) => {
                 </div>
                 <div className="flex flex-col items-center pb-10">
                     <img className="mb-3 w-24 h-24 rounded-full shadow-lg" src={picture} alt=""></img>
-                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
-                        <span className="text-md text-gray-500 dark:text-gray-400">"{comment}"</span>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
+                    <span className="text-md text-gray-500 dark:text-gray-400">"{comment}"</span>
+                    <span className='text-yellow-600 pt-2'>
+                        <FontAwesomeIcon className='px-1' icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon className='px-1' icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon className='px-1' icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon className='px-1' icon={faStar}></FontAwesomeIcon>
+                        <FontAwesomeIcon className='pl-1 text-gray-600' icon={faStar}></FontAwesomeIcon>
+                    </span>
                 </div>
             </div>
         </div>
